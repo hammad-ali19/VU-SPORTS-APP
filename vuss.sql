fde06dd1547955542a21c6ed0df7a05d76bce043
Hammad <chh68815@gmail.com>
2026-03-25 12:12:48 +0500

    updated database

--- vuss.sql
+++ vuss.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2026 at 02:39 AM
+-- Generation Time: Mar 25, 2026 at 08:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `vuss`
 --
-CREATE DATABASE IF NOT EXISTS vuss;
-USE vuss;
+
 -- --------------------------------------------------------
 
 --
@@ -56,7 +55,7 @@ CREATE TABLE `alembic_version` (
 --
 
 INSERT INTO `alembic_version` (`version_num`) VALUES
-('d1ec07a4387e');
+('89bdab385fcd');
 
 -- --------------------------------------------------------
 
@@ -76,9 +75,23 @@ CREATE TABLE `coaches` (
 --
 
 INSERT INTO `coaches` (`id`, `expertise`, `availability`, `user_id`) VALUES
-(5, 'Coach Expertise goes here', 1, 20),
 (6, 'Coach Expertise goes here', 1, 46),
 (7, 'Coach Expertise goes here', 1, 48);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `event_type` text DEFAULT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `sport_id` int(11) DEFAULT NULL,
+  `venue_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -138,29 +151,29 @@ CREATE TABLE `participants_sports` (
 --
 
 INSERT INTO `participants_sports` (`id`, `status`, `approved_by`, `participant_id`, `sport_id`) VALUES
-(1, 'active', 5, 5, 2),
-(2, 'pending', NULL, 5, 1),
-(8, 'active', 5, 10, 2),
+(1, 'pending', NULL, 5, 2),
+(2, 'active', 6, 5, 1),
+(8, 'pending', NULL, 10, 2),
 (9, 'pending', NULL, 11, 3),
 (10, 'pending', NULL, 12, 1),
 (11, 'pending', NULL, 12, 3),
 (12, 'pending', NULL, 13, 1),
-(13, 'active', 5, 13, 2),
-(15, 'active', 5, 15, 2),
-(19, 'active', 5, 18, 2),
+(13, 'pending', NULL, 13, 2),
+(15, 'pending', NULL, 15, 2),
+(19, 'pending', NULL, 18, 2),
 (22, 'pending', NULL, 21, 2),
 (23, 'pending', NULL, 21, 3),
 (26, 'pending', NULL, 23, 2),
 (27, 'pending', NULL, 23, 3),
 (28, 'pending', NULL, 24, 1),
-(29, 'active', 5, 24, 2),
+(29, 'pending', NULL, 24, 2),
 (35, 'pending', NULL, 28, 2),
 (36, 'pending', NULL, 29, 1),
-(37, 'pending', NULL, 30, 1),
+(37, 'active', 6, 30, 1),
 (38, 'pending', NULL, 31, 2),
-(39, 'pending', NULL, 32, 1),
+(39, 'active', 6, 32, 1),
 (40, 'pending', NULL, 33, 2),
-(41, 'pending', NULL, 34, 1);
+(41, 'active', 6, 34, 1);
 
 -- --------------------------------------------------------
 
@@ -182,8 +195,43 @@ CREATE TABLE `sports` (
 
 INSERT INTO `sports` (`id`, `name`, `description`, `max_participants`, `coach_id`) VALUES
 (1, 'Cricket', NULL, NULL, 6),
-(2, 'Badminton', NULL, NULL, 5),
+(2, 'Badminton', NULL, NULL, NULL),
 (3, 'VollyBall', NULL, NULL, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `max_participants` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `sport_id` int(11) NOT NULL,
+  `coach_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `teams`
+--
+
+INSERT INTO `teams` (`id`, `name`, `max_participants`, `created_at`, `sport_id`, `coach_id`) VALUES
+(4, 'team a', 11, '2026-03-24 08:32:44', 1, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_participants`
+--
+
+CREATE TABLE `team_participants` (
+  `id` int(11) NOT NULL,
+  `joined_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `team_id` int(11) NOT NULL,
+  `participant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -209,12 +257,11 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `role`) VALUES
 (7, 'admin', 'admin@example.com', '123', 'ACTIVE', 'ADMIN'),
 (9, 'Zubair Ali', 'test@mail.com', '123', 'ACTIVE', 'PARTICIPANT'),
 (19, 'Jacob', 'jacob@example.com', '123', 'ACTIVE', 'PARTICIPANT'),
-(20, 'Fareed', 'fareed@example.com', '123', 'ACTIVE', 'COACH'),
 (22, 'Zameer', 'zameer@example.com', '123', 'ACTIVE', 'PARTICIPANT'),
-(23, 'Rashid Azim', 'rashid@example.com', '123', 'ACTIVE', 'PARTICIPANT'),
+(23, 'Rashid Azim', 'rashid@example.com', '123', 'BLOCKED', 'PARTICIPANT'),
 (26, 'Junaid', 'junaid@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
 (28, 'Tariq', 'tariq@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
-(31, 'Faisal', 'faisal@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
+(31, 'Faisal', 'faisal@example.com', '123456', 'BLOCKED', 'PARTICIPANT'),
 (34, 'Jahangir', 'jahangir@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
 (36, 'Musa', 'musa@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
 (37, 'Asif', 'asif@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
@@ -223,10 +270,29 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `role`) VALUES
 (44, 'Tabasum', 'tabasum@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
 (45, 'Raheel', 'raheel@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
 (46, 'Ali', 'ali@example.com', '123456', 'ACTIVE', 'COACH'),
-(48, 'Jawad', 'jawad@example.com', '123456', 'PENDING', 'COACH'),
+(48, 'Jawad', 'jawad@example.com', '123456', 'ACTIVE', 'COACH'),
 (49, 'Aoun', 'aoun@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
 (50, 'Zeeshan', 'zeshan@example.com', '123456', 'ACTIVE', 'PARTICIPANT'),
-(51, 'Uzair', 'uzair@example.com', '123456', 'BLOCKED', 'PARTICIPANT');
+(51, 'Uzair', 'uzair@example.com', '123456', 'ACTIVE', 'PARTICIPANT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venues`
+--
+
+CREATE TABLE `venues` (
+  `id` int(11) NOT NULL,
+  `location` text NOT NULL,
+  `availability` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `venues`
+--
+
+INSERT INTO `venues` (`id`, `location`, `availability`) VALUES
+(8, 'venue 1', 1);
 
 --
 -- Indexes for dumped tables
@@ -253,6 +319,15 @@ ALTER TABLE `coaches`
   ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `date_time` (`date_time`),
+  ADD KEY `sport_id` (`sport_id`),
+  ADD KEY `venue_id` (`venue_id`);
+
+--
 -- Indexes for table `participants`
 --
 ALTER TABLE `participants`
@@ -277,11 +352,33 @@ ALTER TABLE `sports`
   ADD UNIQUE KEY `coach_id` (`coach_id`);
 
 --
+-- Indexes for table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `coach_id` (`coach_id`),
+  ADD KEY `sport_id` (`sport_id`);
+
+--
+-- Indexes for table `team_participants`
+--
+ALTER TABLE `team_participants`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_team_participant` (`team_id`,`participant_id`),
+  ADD KEY `participant_id` (`participant_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `venues`
+--
+ALTER TABLE `venues`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -300,16 +397,22 @@ ALTER TABLE `coaches`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `participants`
 --
 ALTER TABLE `participants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `participants_sports`
 --
 ALTER TABLE `participants_sports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `sports`
@@ -318,10 +421,28 @@ ALTER TABLE `sports`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `team_participants`
+--
+ALTER TABLE `team_participants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `venues`
+--
+ALTER TABLE `venues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
@@ -340,6 +461,13 @@ ALTER TABLE `coaches`
   ADD CONSTRAINT `coaches_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `events_ibfk_2` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`) ON DELETE SET NULL;
+
+--
 -- Constraints for table `participants`
 --
 ALTER TABLE `participants`
@@ -349,15 +477,29 @@ ALTER TABLE `participants`
 -- Constraints for table `participants_sports`
 --
 ALTER TABLE `participants_sports`
-  ADD CONSTRAINT `participants_sports_ibfk_1` FOREIGN KEY (`approved_by`) REFERENCES `coaches` (`id`),
   ADD CONSTRAINT `participants_sports_ibfk_2` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `participants_sports_ibfk_3` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `participants_sports_ibfk_3` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `participants_sports_ibfk_4` FOREIGN KEY (`approved_by`) REFERENCES `coaches` (`id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `sports`
 --
 ALTER TABLE `sports`
   ADD CONSTRAINT `sports_ibfk_1` FOREIGN KEY (`coach_id`) REFERENCES `coaches` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `teams`
+--
+ALTER TABLE `teams`
+  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`coach_id`) REFERENCES `coaches` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `teams_ibfk_2` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `team_participants`
+--
+ALTER TABLE `team_participants`
+  ADD CONSTRAINT `team_participants_ibfk_1` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `team_participants_ibfk_2` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
